c66c60575ed6f18bb5ecb7a2ffe699423c6e7438
CosmoWolf <kosmowolf@gmail.com>
2021-05-18 22:59:27 +0300

    Java Core - измения 02

---
+++ src/main/java/lesson02/Main2.java
@@ -0,0 +1,12 @@
+package lesson02;
+
+
+public class Main2 {
+    static String classCount="Hello";
+   // static  String helSasha =
+
+          public static void main(String[] args) {
+              System.out.println(classCount);
+//        System.out.println(args);
+          }
+    }

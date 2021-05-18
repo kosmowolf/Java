ae3f481d419ff16a1689c3e850a774cb1de593b2
CosmoWolf <kosmowolf@gmail.com>
2021-05-18 22:55:08 +0300

    Java Core - измения 02

---
+++ src/main/java/HomeWork02/Main.java
@@ -0,0 +1,5 @@
+package HomeWork02;
+
+public class Main {
+
+}
--- src/main/java/lesson01/Main.java
+++ src/main/java/lesson01/Main.java
@@ -1,71 +1,75 @@
 package lesson01;
 
 public class Main {
-    //1. Создать пустой проект в IntelliJ IDEA и прописать метод main().
+
+    //служебные слова | что вернуть | название метода (параметры для метода) { тело метода }
+//    public static void main(String[] args) {
+        //ТипПеременной имяПеременной(идентификатор) = значение;
+//        int i = 1123123;
+//        double d = 123.11;
+//        char c = 'П';
+//        boolean b = true; //false
+//        String s = "Привет, мир!";
+//        System.out.println(s);
+
+        //ТипПеременной имяПеременной(идентификатор) = значение;
+//        Object helloWorld = new String("Привет, мир!");
+//        System.out.println(helloWorld);
+
+//        int ostatok = 10%3;
+//        System.out.println(ostatok);
+
+//        int chislo = 1;
+//        System.out.println(chislo);
+//        chislo = 2;
+//        System.out.println(chislo);
+//        chislo = chislo + 1;
+//        System.out.println(chislo);
+//        chislo += 1;
+//        System.out.println(chislo);
+//        chislo++;
+//        System.out.println(chislo);
+//        ++chislo;
+//        System.out.println(chislo);
+
+//        System.out.println(chislo++);
+//        System.out.println(++chislo);
+
+//        if (chislo == 8) {
+//            System.out.println("Число = 8");
+//        } else if (chislo == 7){
+//            System.out.println("Число = 7");
+//        } else {
+//            System.out.println("Число точно != 7 или 8");
+//        }
+//    }
 
     public static void main(String[] args) {
-
-        //2. Создать переменные всех пройденных типов данных и инициализировать их значения.
-        short sVal = 12442;
-        int iVal = 1000;
-        long lVal = 200000L;
-        float fVal = 12.23f;
-        double dVal = -123.123;
-        char cVal = '*';
-        boolean bVal = false;
-
-        //3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
-        //   где a, b, c, d – аргументы этого метода, имеющие тип float.
-        System.out.println("3. " + formula(2.3f, 3.5f, 4.6f, 5.9f)); //3. вызов метода
-
-        //4. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит
-        //   в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
-        System.out.println("4. " + sum(5, 20));
-
-        //5. Написать метод, которому в качестве параметра передается целое число,
-        //  метод должен напечатать в консоль, положительное ли число передали или отрицательное.
-        //  Замечание: ноль считаем положительным числом.
-        System.out.println("5. " + checkInt(-1));
-        //6. Написать метод, которому в качестве параметра передается целое число.
-        //  Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
-        System.out.println("6. "+checkNegative(-2));
-
-        //7. Написать метод, которому в качестве параметра передается строка, обозначающая имя.
-        // Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
-        System.out.println(helloName("Петр!"));
+        System.out.println("значение сложения = " + (1+1));
+        printHelloWorld();
+        String hello = "Еще раз привет!";
+        printHelloWorld(hello);
+        String newString = printHelloWorldAndReturn("Одна строка");
+        System.out.println(newString);
+        System.out.println(sum(1, 4));
     }
 
-    private static String helloName(String s) {//7.
-        return "Привет, " + s;
+    private static void printHelloWorld() {
+        System.out.println("Привет, мир!");
     }
 
-    private static boolean checkNegative(int j) {//6.
-        if (j < 0) {
-            return true;
-        } else {
-            return false;
-        }
+    private static void printHelloWorld(String s) {
+        System.out.println(s);
     }
 
-    private static String checkInt(int i) {//5.
-        if (i < 0) {
-            return "Отрицательное";
-        } else {
-            return "Положительное";
-        }
+    private static String printHelloWorldAndReturn(String s) {
+        System.out.println(s);
+        return s + s;
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
     }
 
 
-    public static float formula(float a, float b, float c, float d) {//3.
-        float formula = a * (b + (c / d));
-        return formula;
-    }
-
-    public static boolean sum(int a, int b) {//4.
-        if (a + b >= 10 & 20 >= a + b) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }

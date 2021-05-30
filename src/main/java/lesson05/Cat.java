378109c43d60cce07e23bed1be18c319454e7e9a
CosmoWolf <kosmowolf@gmail.com>
2021-05-30 23:22:16 +0300

    Java Core - лекция к 5 уроку

--- src/main/java/lesson05/Cat.java
+++ src/main/java/lesson05/Cat.java
@@ -1,12 +1,78 @@
 package lesson05;
 
 public class Cat {
-    public String name;
-    public  String color;
-    public int age;
+    private static int catCount = 0;
+    private static String description = "Божество, которое много орет и всегда просит еды";
+    private String name;
+    private String color;
+    private int age;
 
-    public void meow(){
-        System.out.printf("Кот %s говорит May\n", name);
+    public static int getCatCount() {
+        return catCount;
+    }
 
+    public static String getDescription() {
+        return description;
+    }
+
+    public Cat(String color, int age) {
+        this("Уличный", color, age);
+    }
+
+    public Cat(String name, String color, int age) {
+        catCount++;
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void meow() {
+        meow(false);
+    }
+
+    public void meow(boolean isLong) {
+        if (isLong) {
+            System.out.printf("Кот %s говорит: Мяяяяяяяяяяяяяу!\n", name);
+        } else {
+            System.out.printf("Кот %s говорит: Мяу!\n", name);
+        }
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age<0) {
+            System.err.println("Неверно указан возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
--- src/main/java/lesson05/Main.java
+++ src/main/java/lesson05/Main.java
@@ -2,16 +2,49 @@ package lesson05;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat();
-        cat1.name = "Вася";
-        cat1.color = "Рыжий";
-        cat1.age = 10;
-        cat1.meow();
+        int a = 5;
+        int b = a;
+//        int c = a;
+//        c++;
+        increment(a);
+        System.out.println(a + " " + b);
 
-        Cat cat2 = new Cat();
-        cat2.name = "Пушистик";
-        cat2.color = "Белый";
-        cat2.age = 5;
+        System.out.println(Cat.getDescription());
+        System.out.println("Котов: " + Cat.getCatCount());
+
+        Cat cat1 = new Cat("Васька", "Рыжий", 10);
+        //Какая-то логика заставившая кота сменить цвет
+//        cat1.setColor("Грязный");
+//        cat1.setAge(10);
+        cat1.meow(true);
+        System.out.println(cat1);
+
+        Cat cat2 = new Cat("Белый", 5);
+        cat2.setName("Пушистик");
+        cat2.setColor("Белый");
+        cat2.setAge(5);
         cat2.meow();
+
+        System.out.println("Котов: " + Cat.getCatCount());
+
+
+//        Cat catCopy = cat2;
+
+//        changeName(catCopy);
+//        incrementAge(catCopy);
+
+        System.out.println(cat1.getAge());
+    }
+
+    private static void increment(int i) {
+        i++;
+    }
+
+    private static void changeName(Cat cat) {
+//        cat.name = cat.name + " Первый";
+    }
+
+    private static void incrementAge(Cat cat) {
+//        cat.age++;
     }
 }
